8cb65241b8f838424c56a00f8e1596a9cd6fbb09
vmsou <73619111+vmsou@users.noreply.github.com>
2021-05-08 13:35:48 -0300

    Initial Upload

---
+++ core.py
@@ -0,0 +1,11 @@
+class BadAlloc(Exception):
+    pass
+
+
+class Byte:
+    def __init__(self):
+        self.value = None
+
+    def __repr__(self):
+        return f"{hex(id(self))}: {self.value}"
+
---
+++ mem.py
@@ -0,0 +1,76 @@
+import sys
+
+import numpy as np
+from core import BadAlloc, Byte
+
+
+class Bucket:
+    size = 4096
+
+    def __init__(self):
+        self._data = Byte()
+
+    def __repr__(self):
+        return f"{hex(id(self))}"
+
+    def __iter__(self):
+        yield self.data.value
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self.data.value = value
+
+
+class Heap:
+    n_buckets = int(input("Number of columns: "))
+    n_rows = int(input("Number of rows: "))
+    buckets = np.ndarray((n_rows, n_buckets), dtype=object)
+    buckets_used = np.zeros((n_rows, n_buckets))
+
+    def __init__(self):
+        self._start()
+
+    def allocate(self, obj):
+        if sys.getsizeof(obj) > Bucket.size:
+            raise BadAlloc("Object too heavy.")
+        for i in range(Heap.n_rows):
+            for j in range(Heap.n_buckets):
+                if not self.buckets_used[i][j]:
+                    self.buckets_used[i][j] = 1
+                    self.buckets[i][j].data = obj
+                    return self.buckets[i][j]
+        raise BadAlloc("Not enough space.")
+
+    def free(self, p):
+        for i in range(Heap.n_rows):
+            for j in range(Heap.n_buckets):
+                if self.buckets[j][i] == p:
+                    self.buckets_used[j][i] = 0
+                    return
+
+    def _start(self):
+        for i in range(Heap.n_rows):
+            for j in range(Heap.n_buckets):
+                Heap.buckets[i][j] = Bucket()
+
+
+heap = Heap()
+
+
+def new(obj):
+    return heap.allocate(obj)
+
+
+def delete(p):
+    return heap.free(p)
+
+
+print(heap.buckets_used)
+t = new(5)
+print(heap.buckets_used)
+print(t)
+print(*t)
