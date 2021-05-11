696444d04af82b44532946bd55cb5afc0a9793d5
Vinicius Marques <viniciusmarcs@icloud.com>
2021-05-11 15:47:08 -0300

    Memory Allocation - CPP implementation fixed

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,14 +1,15 @@
 extern "C" {
-    void first(const void* indatav, int blocks, int rows, int columns) {
+    void first(void* indatav, int blocks, int rows, int columns) {
         bool* indata = (bool*) indatav;
         int count = 0;
         for (int i = 0; i < rows * columns; i++) {
-           if (indata[i] == false) {
+           if (indata[i] == 0) {
                count++;
            } else count = 0;
+
            if (count >= blocks) {
-               for (int j = i - count; j < blocks + i; j++) {
-                   indata[j] = true;
+               for (int j = blocks - 1; j > -1; j--) {
+                   indata[i - j] = 1;
                }
                break;
            }
--- cpp/test.py
+++ cpp/test.py
@@ -3,15 +3,17 @@ import numpy as np
 
 rows = 3
 columns = 3
-blocks = 2
-indata = np.random.choice([True, False], (rows, columns), p=[0.2, 0.8]).astype(c_bool)
+blocks = 5
+indata = np.random.choice([True, False], (rows, columns), p=[0, 1]).astype(np.bool)
 lib = cdll.LoadLibrary('./libtest.so')
 first = lib.first
 
-print(indata.astype(c_int))
+print(indata.sum())
+print(indata.astype(int))
 first.argtypes = [c_void_p, c_int, c_int, c_int]
 first.restype = c_int
 first(indata.ctypes.data, blocks, rows, columns)
 
 print()
-print(indata.astype(c_int))
+print(indata.astype(np.int))
+print(indata.sum())
