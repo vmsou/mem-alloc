a98e093f594e8532ec46a0b7307d798160fbc3e0
Vinicius Marques <viniciusmarcs@icloud.com>
2021-05-11 15:48:59 -0300

    Memory Allocation - CPP testing

--- cpp/test.py
+++ cpp/test.py
@@ -1,9 +1,9 @@
 from ctypes import *
 import numpy as np
 
-rows = 3
-columns = 3
-blocks = 5
+rows = 5000
+columns = 5000
+blocks = 1_000_000
 indata = np.random.choice([True, False], (rows, columns), p=[0, 1]).astype(np.bool)
 lib = cdll.LoadLibrary('./libtest.so')
 first = lib.first
@@ -11,7 +11,6 @@ first = lib.first
 print(indata.sum())
 print(indata.astype(int))
 first.argtypes = [c_void_p, c_int, c_int, c_int]
-first.restype = c_int
 first(indata.ctypes.data, blocks, rows, columns)
 
 print()
