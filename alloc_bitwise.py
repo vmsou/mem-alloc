a144381138e5ad627b7e857258a6288042fc00f9
vmsou <viniciusmarcs@icloud.com>
2021-05-19 20:10:53 -0300

    Memory Allocation Bitwise - best fit fixed

--- alloc_bitwise.py
+++ alloc_bitwise.py
@@ -6,8 +6,8 @@ rows = 5
 columns = 20
 # blocks_used = np.random.choice([0, 1], (rows, columns), p=[0.8, 0.2])
 blocks_used = np.zeros((rows, columns))
-# bytes_map = np.random.choice([30, 60], (rows, columns))
-bytes_map = np.repeat(30, rows * columns).reshape((rows, columns))
+bytes_map = np.random.choice([30, 60], (rows, columns))
+# bytes_map = np.repeat(30, rows * columns).reshape((rows, columns))
 b = range(rows * columns)
 
 
@@ -54,7 +54,7 @@ def best_bitwise(min_bytes):
             count = 0
             soma = 0
 
-        if count <= lowest_count and lowest_sum > soma >= min_bytes:
+        if count <= lowest_count and lowest_sum >= soma >= min_bytes:
             lowest_sum = soma
             lowest_count = count
             lowest_idx = i
@@ -162,7 +162,7 @@ def main():
 
     # new(90, fit="first", show=True)
     # new(90, fit="best", show=True)
-    new(120, fit="worst", show=True)
+    new(90, fit="best", show=True)
 
 
 if __name__ == '__main__':
